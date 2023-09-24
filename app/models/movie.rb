ce6d336d443f29f24565e98638f3032373c5b0e4
cuilu fan <cuilu.fan@gmail.com>
2023-09-24 12:45:04 +0200

    top banner added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   # A movie must have a unique title and an overview.
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
